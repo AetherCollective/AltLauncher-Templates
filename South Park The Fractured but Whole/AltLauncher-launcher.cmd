beffa8c3b1c10aedf8ecdc6bfe4fccff99478ce3
Aether Collective <EmilyLove@writeme.com>
2026-01-30 14:13:16 -0500

    Refactors Templates
    Cleaned up unused components of templates. Switched from hard coded Program Files to using the environment variable.

--- South Park The Fractured but Whole/AltLauncher-launcher.cmd
+++ South Park The Fractured but Whole/AltLauncher-launcher.cmd
@@ -1,6 +1,6 @@
 tasklist | find /i "upc.exe" >nul 2>&1
 IF ERRORLEVEL 1 (
 Echo Launching Ubisoft Connect *REQUIRED*...
-"C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\UbisoftConnect.exe"
+"%ProgramFiles(x86)%\Ubisoft\Ubisoft Game Launcher\UbisoftConnect.exe"
 timeout /T 15 /NOBREAK >Nul
 )
